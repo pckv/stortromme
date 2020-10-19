8adf6457efc1fe8ef05a9ac5a196b9f955861230
Per Christian Kvalvik <pc@kvalvik.no>
2020-10-19 21:03:08 +0200

    Refactor properties to uppercase

--- Services/PlayerService.cs
+++ Services/PlayerService.cs
@@ -2,13 +2,14 @@ using broken_picturephone_blazor.Data;
 
 namespace broken_picturephone_blazor.Services {
     public class PlayerService {
-        public Player player { get; set; }
-        public Lobby lobby { get; set; }
+        public Player Player { get; set; }
+        public Lobby Lobby { get; set; }
+        public bool WasKicked { get; set; }
 
         public void Clear()
         {
-            this.player = null;
-            this.lobby = null;
+            this.Player = null;
+            this.Lobby = null;
         }
     }
 }
