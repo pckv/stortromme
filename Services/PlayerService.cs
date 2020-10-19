0d0c604b9fe4fddbdc447ade0ea9bdc8001b0902
Per Christian Kvalvik <pc@kvalvik.no>
2020-10-19 20:00:33 +0200

    Refactor us to player

--- Services/PlayerService.cs
+++ Services/PlayerService.cs
@@ -2,12 +2,12 @@ using broken_picturephone_blazor.Data;
 
 namespace broken_picturephone_blazor.Services {
     public class PlayerService {
-        public Player us { get; set; }
+        public Player player { get; set; }
         public Lobby lobby { get; set; }
 
         public void Clear()
         {
-            this.us = null;
+            this.player = null;
             this.lobby = null;
         }
     }
