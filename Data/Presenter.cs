bfa128712ac5b96033de58b323524c5e311b7826
Per Christian Kvalvik <pc@kvalvik.no>
2020-12-24 16:22:20 +0100

    Add flipping book for screenshots

--- Data/Presenter.cs
+++ Data/Presenter.cs
@@ -4,5 +4,13 @@ namespace stortromme.Data
     {
         public Book Book { get; set; }
         public int DisplayPages { get; set; }
+        public bool Flipped { get; set; }
+
+        public Presenter(Book book)
+        {
+            Book = book;
+            DisplayPages = 1;
+            Flipped = false;
+        }
     }
 }
