6c7bd247032350485c47f03de90cca679de51425
Per Christian Kvalvik <pc@kvalvik.no>
2020-12-27 17:26:23 +0100

    Improve login form

--- Data/LobbyForm.cs
+++ Data/LobbyForm.cs
@@ -2,11 +2,11 @@ using System.ComponentModel.DataAnnotations;
 
 public class LobbyForm
 {
-    [Required]
+    [Required(ErrorMessage = "Lobby name is required")]
     [StringLength(32, ErrorMessage = "Lobby name must not exceed 32 characters")]
     public string LobbyName { get; set; } 
 
-    [Required]
+    [Required(ErrorMessage = "Player name is required")]
     [StringLength(32, ErrorMessage = "Player name must not exceed 32 characters")]
     public string PlayerName { get; set; } 
 }
